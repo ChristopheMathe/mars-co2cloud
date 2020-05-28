ba9db458df186c2ecaaf5a10a0f73118829365a0
Christophe Mathé <serakhiel91@gmail.com>
2020-05-28 15:30:43 +0200

    extract_profile: add the directory outputc

--- extract_profile_from_simu/extract_profile.py
+++ extract_profile_from_simu/extract_profile.py
@@ -1,8 +1,8 @@
 #!/bin/bash python3
 
-from numpy import abs, argmin, argmax, savetxt, c_, append, unravel_index
+from numpy import abs, amax, argmin, argmax, savetxt, c_, append, unravel_index
 from netCDF4 import Dataset
-from os import listdir
+from os import listdir, mkdir
 
 
 def main():
@@ -37,7 +37,7 @@ def main():
     data_latitude = bigdata.variables['latitude']
 
     # choose the solar longitude and latitude for the extraction
-    target_ls = float(input('Select the ls for the extraction: '))
+    target_ls = float(input('Select the ls for the extraction (max='+str(amax(data_ls))+'): '))
     target_latitude = float(input('Select the latitude for the extraction: '))
     target_mmr_max = input('Select the longitude where the mmr is max? (y/n): ')
 
@@ -55,6 +55,12 @@ def main():
         idx_max = argmax(data)  # along longitude
         tmp, idx_longitude = unravel_index(idx_max, data.shape)
 
+    directory_output = 'profiles_'+filename[:-3]+'_ls'+str(int(data_ls[idx_ls]))+'_lat'+str(int(data_latitude[
+                                                                                                idx_latitude]))+'N/'
+    try:
+        mkdir(directory_output)
+    except:
+        print('Be careful, the folder already exist')
 
     # extract data at longitude 0°E
     for i, value_i in enumerate(list_var):
@@ -69,28 +75,28 @@ def main():
 
         # write the extracted data in file
         if value_i == 'temp':
-            savetxt('profile', c_[data], fmt=format_output)
+            savetxt(directory_output+'profile', c_[data], fmt=format_output)
 
         elif value_i == 'ccnq':
-            savetxt('profile_ccn_mass', c_[data], fmt=format_output)
+            savetxt(directory_output+'profile_ccn_mass', c_[data], fmt=format_output)
 
         elif value_i == 'ccnN':
-            savetxt('profile_ccn_number', c_[data], fmt=format_output)
+            savetxt(directory_output+'profile_ccn_number', c_[data], fmt=format_output)
 
         elif value_i == 'ccnqco2':
-            savetxt('profile_ccnco2_mass', c_[data], fmt=format_output)
+            savetxt(directory_output+'profile_ccnco2_mass', c_[data], fmt=format_output)
 
         elif value_i == 'ccnNco2':
-            savetxt('profile_ccnco2_number', c_[data], fmt=format_output)
+            savetxt(directory_output+'profile_ccnco2_number', c_[data], fmt=format_output)
 
         elif value_i == 'dustq':
-            savetxt('profile_dust_mass', c_[data], fmt=format_output)
+            savetxt(directory_output+'profile_dust_mass', c_[data], fmt=format_output)
 
         elif value_i == 'dustN':
-            savetxt('profile_dust_number', c_[data], fmt=format_output)
+            savetxt(directory_output+'profile_dust_number', c_[data], fmt=format_output)
 
         else:
-            savetxt('profile_'+str(value_i), c_[data], fmt=format_output)
+            savetxt(directory_output+'profile_'+str(value_i), c_[data], fmt=format_output)
 
 if '__main__' == __name__:
     main()
