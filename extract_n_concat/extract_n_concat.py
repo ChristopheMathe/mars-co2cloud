61cf598ff2c91d456b7003d89a91bedccb1c8e0f
Christophe Mathé <serakhiel91@gmail.com>
2020-05-29 15:15:34 +0200

    WIP: extract_n_concat.py

--- extract_n_concat/extract_n_concat.py
+++ extract_n_concat/extract_n_concat.py
@@ -10,12 +10,13 @@ def main():
   #              'h2o_vap', 'h2o_ice', 'tau', 'dustq', 'dustN', 'h2o_ice_s', 'co2conservation'] # riceco2
 
     list_files = listdir('.')
-    list_diagfi = [name for name in enumerate(list_files) if 'diagfi' in name]
+    list_diagfi = [name for b, name in enumerate(list_files) if 'diagfi' in name]
     print(list_diagfi)
 
+    list_diagfi.sort()
     # Extraction: ncks -v var1, var2, diagfi1.nc extract.nc
-    for name, i in enumerate(list_diagfi):
-        print(name, i)
+    for i, name in enumerate(list_diagfi):
+        print(i, name)
         subprocess.run(["ncks", "-v", ','.join(list_var), name, "extraction/extracted_from_"+name[:-3]+".nc"])
 
 
