3bd458cf7a5380af13f83c08108639ed62a6a8d4
Christophe Mathé <serakhiel91@gmail.com>
2020-05-29 15:07:30 +0200

    WIP: extract_n_concat.py

--- extract_n_concat/extract_n_concat.py
+++ extract_n_concat/extract_n_concat.py
@@ -1,14 +1,22 @@
 #!/bin/bash python3
 import subprocess
+from os import listdir
+
 
 def main():
-    list_var = ['Time', 'aire', 'phisinit', 'controle', 'aps', 'bps', 'ap', 'bp',  # obligatoire
-                'Ls', 'Sols', 'temp', 'ps', 'tsurf', 'pressure',# basics
-                'satuco2', 'co2_ice', 'co2', 'ccnqco2', 'ccnNco2', 'co2_ice', 'ccnq', 'ccnN', 'precip_co2_ice',
-                'h2o_vap', 'h2o_ice', 'tau', 'dustq', 'dustN', 'h2o_ice_s', 'co2conservation'] # riceco2
+    list_var = ['Time', 'aire', 'phisinit', 'controle', 'aps', 'bps', 'ap', 'bp']  # obligatoire
+#                'Ls', 'Sols', 'temp', 'ps', 'tsurf', 'pressure',# basics
+ #               'satuco2', 'co2_ice', 'co2', 'ccnqco2', 'ccnNco2', 'co2_ice', 'ccnq', 'ccnN', 'precip_co2_ice',
+  #              'h2o_vap', 'h2o_ice', 'tau', 'dustq', 'dustN', 'h2o_ice_s', 'co2conservation'] # riceco2
+
+    list_files = listdir('.')
+    list_diagfi = [name for name in enumerate(list_files) if 'diagfi' in name]
+    print(list_diagfi)
 
     # Extraction: ncks -v var1, var2, diagfi1.nc extract.nc
-    subprocess.run(["ncks", "-v", ','.join(list_var), "diagfi1.nc", "extraction/extracted_from_diagfi1.nc"])
+    for name, i in enumerate(list_diagfi):
+        print(name, i)
+        subprocess.run(["ncks", "-v", ','.join(list_var), name, "extraction/extracted_from_"+name[:-3]+".nc"])
 
 
     # concat all files from the extraction
