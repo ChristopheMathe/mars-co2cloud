cf208192e15edc98ea98ff844700fd4b22ce78e8
Christophe Mathé <serakhiel91@gmail.com>
2020-06-15 14:43:45 +0200

    WIP: extract_n_concat.py

--- extract_n_concat/extract_n_concat.py
+++ extract_n_concat/extract_n_concat.py
@@ -4,10 +4,10 @@ from os import listdir
 
 
 def main():
-    list_var = ['Time', 'aire', 'phisinit', 'controle', 'aps', 'bps', 'ap', 'bp']  # obligatoire
-#                'Ls', 'Sols', 'temp', 'ps', 'tsurf', 'pressure',# basics
- #               'satuco2', 'co2_ice', 'co2', 'ccnqco2', 'ccnNco2', 'co2_ice', 'ccnq', 'ccnN', 'precip_co2_ice',
-  #              'h2o_vap', 'h2o_ice', 'tau', 'dustq', 'dustN', 'h2o_ice_s', 'co2conservation'] # riceco2
+    list_var = ['Time', 'aire', 'phisinit', 'controle', 'aps', 'bps', 'ap', 'bp' # obligatoire
+                'Ls', 'Sols', 'temp', 'ps', 'tsurf', 'pressure',# basics
+                'satuco2', 'co2_ice', 'co2', 'ccnqco2', 'ccnNco2', 'co2_ice', 'ccnq', 'ccnN', 'precip_co2_ice',
+                'h2o_vap', 'h2o_ice', 'tau', 'dustq', 'dustN', 'h2o_ice_s', 'co2conservation'] # riceco2
 
     list_files = listdir('.')
     list_diagfi = [name for b, name in enumerate(list_files) if 'diagfi' in name]
