a62fdbbeb1c851ffe20e33e00da3acc4a176aece
Christophe Mathé <serakhiel91@gmail.com>
2020-10-22 17:15:00 +0200

    Improved check_watercycle

--- check_watercycle/check_watercycle.py
+++ check_watercycle/check_watercycle.py
@@ -5,39 +5,57 @@ from matplotlib.colors import DivergingNorm, LogNorm
 from numpy import mean, abs, min, max, zeros, where, ones, concatenate, flip, arange, unravel_index, argmax, array, \
     count_nonzero, diff, std, savetxt, c_, append, loadtxt, asarray, power, linspace, logspace
 
+def correction_value(data, threshold):
+    from numpy import ma
 
-def correction_data(data, threshold):
-    for i in range(data.shape[0]):
-        dim2, dim3 = where(data[i, :, :] <= threshold)
-        data[i, dim2, dim3] = 0
+    data = ma.masked_where(data <= threshold, data)
+
     return data
 
 
-def extract_data(data, zonal_mean=None):
-    data_h2o_ice_s = data.variables['h2o_ice_s'][:,:,:]
-    data_icetot = data.variables['icetot'][:,:,:]
-    data_tauTES = data.variables['tauTES'][:,:,:]
-    data_mtot = data.variables['mtot'][:,:,:]
-    data_ps = data.variables['ps'][:,:,:]
-    data_tsurf = data.variables['tsurf'][:,:,:]
+def rotate_data(*list_data, doflip):
+    from numpy import flip
+    list_data = list(list_data)
 
-    data_h2o_ice_s = correction_data(data_h2o_ice_s, threshold=1e-5)
+    for i, value in enumerate(list_data):
+        list_data[i] = list_data[i].T  # get Ls on x-axis
+        if doflip:
+            list_data[i] = flip(list_data[i], axis=0)  # reverse to get North pole on top of the fig
 
-    data_icetot = correction_data(data_icetot, threshold=1e-5)
+    return list_data
 
-    data_tauTES = correction_data(data_tauTES, threshold=1e-5)
 
-    data_mtot = correction_data(data_mtot, threshold=1e-5)
+def extract_data(data):
+    data_h2o_ice_s = data.variables['h2o_ice_s'][:, :, :]
+    data_icetot = data.variables['icetot'][:, :, :]
+    data_tauTES = data.variables['tauTES'][:, :, :]
+    data_mtot = data.variables['mtot'][:, :, :]
+    data_ps = data.variables['ps'][:, :, :]
+    data_tsurf = data.variables['tsurf'][:, :, :]
+    data_co2ice = data.variables['co2ice'][:,:,:,]
 
-    if zonal_mean:
-        data_h2o_ice_s = flip(mean(data_h2o_ice_s, axis=2).T, axis=0)
-        data_icetot = flip(mean(data_icetot, axis=2).T, axis=0)
-        data_tauTES = flip(mean(data_tauTES, axis=2).T, axis=0)
-        data_mtot = flip(mean(data_mtot, axis=2).T, axis=0)
-        data_ps = flip(mean(data_ps, axis=2).T, axis=0)
-        data_tsurf = flip(mean(data_tsurf, axis=2).T, axis=0)
+    # correct data
+    data_h2o_ice_s = correction_value(data_h2o_ice_s, threshold=1e-13)
+    data_icetot = correction_value(data_icetot, threshold=1e-13)
+    data_tauTES = correction_value(data_tauTES, threshold=1e-13)
+    data_mtot = correction_value(data_mtot, threshold=1e-13)
+    data_ps = correction_value(data_ps, threshold=1e-13)
+    data_tsurf = correction_value(data_tsurf, threshold=1e-13)
+    data_co2ice = correction_value(data_co2ice, threshold=1e-13)
 
-    return data_h2o_ice_s, data_icetot, data_tauTES, data_mtot, data_ps, data_tsurf
+    # compute zonal mean
+    data_h2o_ice_s = mean(data_h2o_ice_s, axis=2)
+    data_icetot = mean(data_icetot, axis=2)
+    data_tauTES = mean(data_tauTES, axis=2)
+    data_mtot = mean(data_mtot, axis=2)
+    data_ps = mean(data_ps, axis=2)
+    data_tsurf = mean(data_tsurf, axis=2)
+    data_co2ice = mean(data_co2ice, axis=2)
+
+    # rotate data
+    data_h2o_ice_s, data_icetot, data_tauTES, data_mtot, data_ps, data_tsurf, data_co2ice = rotate_data(
+        data_h2o_ice_s, data_icetot, data_tauTES, data_mtot, data_ps, data_tsurf, data_co2ice, doflip=True)
+    return data_h2o_ice_s, data_icetot, data_tauTES, data_mtot, data_ps, data_tsurf, data_co2ice
 
 
 def compute_error(data_ref, data):
@@ -49,21 +67,9 @@ def compute_error(data_ref, data):
             if data_ref[i,j] == 0:
                 delta[i,j] == 0
             else:
-                delta[i,j] = (data[i,j] - data_ref[i,j])*100 / data_ref[i,j]
+                delta[i,j] = (data[i,j] - data_ref[i,j]) * 100 / data_ref[i,j]
 
     return delta
-
-
-def linearize_ls(data, dim_time, dim_latitude, interp_time):
-    from numpy import arange
-    from scipy.interpolate import interp2d
-
-    # interpolation to get linear Ls
-    f = interp2d(x=arange(dim_time), y=arange(dim_latitude), z=data, kind='linear')
-
-    data = f(interp_time, arange(dim_latitude))
-
-    return data
 
 
 def plot_figure(data_ref, data, delta, levels, title, unit, format, ndx, data_latitude, savename):
@@ -116,112 +122,88 @@ def plot_figure(data_ref, data, delta, levels, title, unit, format, ndx, data_la
     plt.close(fig)
 
 
-def linear_grid_ls(data):
-    from numpy import linspace, searchsorted
+def get_ls_index(data_time):
+    from numpy import array, searchsorted, max
 
-    axis_ls = linspace(0, 360, data.shape[0])
-    ndx = searchsorted(axis_ls, [0, 90, 180, 270, 360])
-    interp_time = searchsorted(data, axis_ls)
+    axis_ls = array([0, 90, 180, 270, 360])
+    if max(data_time) > 361:
+        # ls = 0, 90, 180, 270, 360
+        idx = searchsorted(data_time[:], [0, 193.47, 371.99, 514.76, 669])
+    else:
+        idx = searchsorted(data_time[:], axis_ls)
 
-    return interp_time, axis_ls, ndx
+    return idx, axis_ls
 
 
 def main():
     # Data from Margaux Vals
-    data_ref = Dataset('/home/mathe/Documents/runs_analysis/simu_ref_cycle_eau_mvals/concat.nc', "r", format="NETCDF4")
-    data_ref_h2o_ice_s, data_ref_icetot, data_ref_tauTES, data_ref_mtot, data_ref_ps, data_ref_tsurf = \
-                                                                                extract_data(data_ref, zonal_mean=True)
+    data_ref = Dataset('../simu_ref_cycle_eau_mvals/concat_vars_3D_Ls.nc', "r", format="NETCDF4")
+    data_ref_h2o_ice_s, data_ref_icetot, data_ref_tauTES, data_ref_mtot, data_ref_ps, data_ref_tsurf, data_ref_co2ice\
+        = extract_data(data_ref)
 
-    data = Dataset('concat_ls_vars_cycle_h2o.nc', "r", format="NETCDF4")
-    data_h2o_ice_s, data_icetot, data_tauTES, data_mtot, data_ps, data_tsurf = extract_data(data, zonal_mean=True)
+    # My data
+    data_3D = Dataset('occigen_test_64x48x32_1years_Tµphy_para_start_simu_ref_Margaux/concat_vars_3D_Ls.nc',
+                      "r",
+                      format="NETCDF4")
+    data_h2o_ice_s, data_icetot, data_tauTES, data_mtot, data_ps, data_tsurf, data_co2ice = extract_data(data_3D)
 
-    data_latitude = data.variables['latitude']
-    data_time = data.variables['Time']
-    interp_time, axis_ls, ndx = linear_grid_ls(data_time)
+    # Get ndx and axis_ls
+    data_latitude = data_3D.variables['latitude']
+    data_latitude = data_latitude[::-1]
+    data_time = data_3D.variables['Time']
+    ndx, axis_ls = get_ls_index(data_time)
 
+    # Compute the difference between Margaux and me
     delta_h2o_ice_s = compute_error(data_ref_h2o_ice_s, data_h2o_ice_s)
-    delta_h2o_ice_s = linearize_ls(delta_h2o_ice_s, data_time.shape[0], data_latitude.shape[0], interp_time)
-
     delta_icetot = compute_error(data_ref_icetot, data_icetot)
-    delta_icetot = linearize_ls(delta_icetot, data_time.shape[0], data_latitude.shape[0], interp_time)
-
     delta_tauTES = compute_error(data_ref_tauTES, data_tauTES)
-    delta_tauTES = linearize_ls(delta_tauTES, data_time.shape[0], data_latitude.shape[0], interp_time)
-
     delta_mtot = compute_error(data_ref_mtot, data_mtot)
-    delta_mtot = linearize_ls(delta_mtot, data_time.shape[0], data_latitude.shape[0], interp_time)
-
     delta_ps = compute_error(data_ref_ps, data_ps)
-    delta_ps = linearize_ls(delta_ps, data_time.shape[0], data_latitude.shape[0], interp_time)
-
     delta_tsurf = compute_error(data_ref_tsurf, data_tsurf)
-    delta_tsurf = linearize_ls(delta_tsurf, data_time.shape[0], data_latitude.shape[0], interp_time)
+    delta_co2ice = compute_error(data_ref_co2ice, data_co2ice)
 
-    # top - left: tsurf
-    print(min(data_tsurf), max(data_tsurf))
+    # Plot: tsurf
+    print('tsurf: min = {:.0f}, max = {:.0f}'.format(min(data_tsurf), max(data_tsurf)))
     plot_figure(data_ref_tsurf, data_tsurf, delta_tsurf, levels=arange(140,270,10),
                 title='Zonal mean of surface temperature', unit='K', format='%.2f', ndx=ndx,
                 data_latitude=data_latitude, savename='check_watercycle_relative_error_tsurf')
 
-    # top - right: ps
-    print(min(data_ps), max(data_ps))
+    # Plot: ps
+    print('ps: min = {:.0f}, max = {:.0f}'.format(min(data_ps), max(data_ps)))
     plot_figure(data_ref_ps, data_ps, delta_ps, levels=arange(300, 1200, 100),
                 title='Zonal mean of surface pressure', unit='Pa', format='%d', ndx=ndx, data_latitude=data_latitude,
                 savename='check_watercycle_relative_error_ps')
 
-    # middle - left: mtot
-    print(min(data_mtot), max(data_mtot))
+    # Plot: mtot
+    print('mtot: min = {:.0f}, max = {:.0f}'.format(min(data_mtot), max(data_mtot)))
     plot_figure(data_ref_mtot, data_mtot, delta_mtot, levels=None,#logspace(-5, -1, 10),
                 title='Zonal mean of total atmospheric mass', unit='kg/m$^2$', format='%.1e', ndx=ndx,
                 data_latitude=data_latitude, savename='check_watercycle_relative_error_mtot')
 
-    # middle - right: icetot
-    print(min(data_icetot), max(data_icetot))
+    # Plot: icetot
+    print('icetot: min = {:.0f}, max = {:.0f}'.format(min(data_icetot), max(data_icetot)))
     plot_figure(data_ref_icetot, data_icetot, delta_icetot, levels=linspace(0, 0.05, 10),
                 title='Zonal mean of total water ice mass', unit='kg/m$^2$', format='%.1e', ndx=ndx,
                 data_latitude=data_latitude, savename='check_watercycle_relative_error_icetot')
 
-    # bot - left: h2o_ice_s
-    print(min(data_h2o_ice_s), max(data_h2o_ice_s))
+    # Plot: h2o_ice_s
+    print('h2o_ice_s: min = {:.0f}, max = {:.0f}'.format(min(data_h2o_ice_s), max(data_h2o_ice_s)))
     plot_figure(data_ref_h2o_ice_s, data_h2o_ice_s, delta_h2o_ice_s, levels=arange(0, 9, 1),
                 title='Zonal mean of surface water ice', unit='kg/m$^2$', format='%d', ndx=ndx,
                 data_latitude=data_latitude, savename='check_watercycle_relative_error_h2o_ice_s')
 
-    # bot - right: tauTES
-    print(min(data_tauTES), max(data_tauTES))
+    # Plot: tauTES
+    print('tauTES: min = {:.0f}, max = {:.0f}'.format(min(data_tauTES), max(data_tauTES)))
     plot_figure(data_ref_tauTES, data_tauTES, delta_tauTES, levels=arange(0, 7, 1),
                 title='Zonal mean of opacity at 825 cm$^{-1}$', unit='', format='%.d',ndx=ndx,
                 data_latitude=data_latitude, savename='check_watercycle_relative_error_tauTES')
 
-
-    try:
-        data_ref = Dataset('/home/mathe/Documents/runs_analysis/simu_ref_cycle_eau_mvals/ref_watercycle_co2ice.nc',
-                           "r", format='NETCDF4')
-    except:
-        print('No ref_watercycle_co2ice.nc exists!')
-        exit()
-
-    try:
-        data = Dataset('concat_co2ice.nc', "r", format='NETCDF4')
-    except:
-        print('No concat_co2ice.nc exists!')
-        exit()
-
-    data_ref_co2ice = data_ref.variables['co2ice'][:,:,:]
-    data_co2ice = data.variables['co2ice'][:,:,:]
-
-    data_ref_co2ice = correction_data(data_ref_co2ice, threshold=1e-5)
-    data_ref_co2ice = flip(mean(data_ref_co2ice, axis=2).T, axis=0)
-
-    data_co2ice = correction_data(data_co2ice, threshold=1e-5)
-    data_co2ice = flip(mean(data_co2ice, axis=2).T, axis=0)
-
-    delta_co2ice = compute_error(data_ref_co2ice, data_co2ice)
-    delta_co2ice = linearize_ls(delta_co2ice, data_time.shape[0], data_latitude.shape[0], interp_time)
-
+    # Plot: co2ice
+    print('co2ice: min = {:.0f}, max = {:.0f}'.format(min(data_co2ice), max(data_co2ice)))
     plot_figure(data_ref_co2ice, data_co2ice, delta_co2ice, levels=None, title='CO$_2$ ice at the surface',
                 unit='kg/m$^2$', format='%.1e', ndx=ndx, data_latitude=data_latitude,
                 savename='check_watercycle_relative_error_co2ice')
+
 
 
 if __name__ == '__main__':
